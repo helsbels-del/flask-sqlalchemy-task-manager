c1f540158dc5352b5d1380176630967237b393d1
Helen Smith <5b64nnhi@students.codeinstitute.net>
2025-01-14 19:26:58 +0000

    FIX: Install different versions to get program to run

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,9 +1,9 @@
 from taskmanager import db
 
 
-class Category(db.model):
+class Category(db.Model):
     # schema for the Category model
-    id = db.Column(db.Interger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
@@ -15,7 +15,7 @@ class Category(db.model):
 
 class Task(db.Model):
     # schema for the Task model
-    id = db.Column(db.Interger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
